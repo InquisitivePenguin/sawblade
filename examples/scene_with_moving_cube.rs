d2b2f351737ece2876f4d36dc03d8ce1ea7ae0cd
Jackson Lewis <st.japa6@gmail.com>
2018-03-26 10:32:05 -0700

    Made example better

--- examples/scene_with_moving_cube.rs
+++ examples/scene_with_moving_cube.rs
@@ -15,6 +15,14 @@ fn build_world() -> Box<World> {
     )
 }
 
+const velocity_decay: f32 = 0.95;
+
+fn decay_velocity(x: f32, y: f32) -> (f32, f32) {
+    let new_x = x * velocity_decay;
+    let new_y = y * velocity_decay;
+    (new_x, new_y)
+}
+
 struct GameWorld {
     cubes: Vec<Cube>,
     pub coordinate_system: CoordinateSystem
@@ -53,8 +61,8 @@ impl World for GameWorld {
 struct Cube {
     coordinates: (u32,u32),
     id: u64,
-    movement_amount_x: i32,
-    movement_amount_y: i32
+    movement_amount_x: f32,
+    movement_amount_y: f32
 }
 
 impl Entity for Cube {
@@ -63,8 +71,8 @@ impl Entity for Cube {
         Cube {
             coordinates,
             id,
-            movement_amount_x: 1,
-            movement_amount_y: 1,
+            movement_amount_x: 1.0,
+            movement_amount_y: 1.0,
         }
     }
     fn get_id(&self) -> u64 {
@@ -73,21 +81,24 @@ impl Entity for Cube {
     fn event(&mut self, world: &mut GameWorld, event: Event) {
         match event {
             Event::Tick => {
-                let move_x = self.movement_amount_x;
-                let move_y = self.movement_amount_y;
+                let new_velocities = decay_velocity(self.movement_amount_x, self.movement_amount_y);
+                self.movement_amount_x = new_velocities.0;
+                self.movement_amount_y = new_velocities.1;
+                let move_x = self.movement_amount_x as i32;
+                let move_y = self.movement_amount_y as i32;
                 self.coordinates = world.coordinate_system.move_to(self.coordinates, move_x, move_y);
             }
             Event::Key(KeyboardKey::Left) => {
-                self.movement_amount_x -= 1;
+                self.movement_amount_x -= 1.0;
             }
             Event::Key(KeyboardKey::Right) => {
-                self.movement_amount_x += 1
+                self.movement_amount_x += 1.0;
             }
             Event::Key(KeyboardKey::Up) => {
-                self.movement_amount_y -= 1;
+                self.movement_amount_y -= 1.0;
             },
             Event::Key(KeyboardKey::Down) => {
-                self.movement_amount_y += 1;
+                self.movement_amount_y += 1.0;
             }
             _ => {}
         }
